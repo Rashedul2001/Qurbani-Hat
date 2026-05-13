1ce29bdb17b246c346a18b94688365521dac56d2
Rashedul2001 <hacker360sr@gmail.com>
2026-05-13 23:32:58 +0600

    Implement Google SignIn

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,18 +3,28 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGO_CLIENT);
-const db = client.db(process.env.MONGO_DB);
+const db = client.db("QurbaniHat");
 
 export const auth = betterAuth({
     database: mongodbAdapter(db, {
         // Optional: if you don't provide a client, database transactions won't be enabled.
         client
     }),
+    baseURL: process.env.BETTER_AUTH_URL,
     emailAndPassword: {
         enabled: true,
         autoSignIn: false,
     },
-    //nextCookies for when I will use the server component to register login
-    // install the nextCookies first
+    socialProviders: {
+        google: {
+            prompt: "select_account",
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+    },
+
+
+
+    //nextCookies for when I will use the server component to register login... install the nextCookies first
     // plugins: [nextCookies()] // make sure this is the last plugin in the array
 });
---
+++ src/lib/services/SocialSignIn.js
@@ -0,0 +1,20 @@
+export const socialSignIn = async (provider) => {
+    const { data, error } = await authClient.signIn.social({
+        provider: provider,
+    }, {
+        onRequest: (ctx) => {
+            setIsLoading(true);
+        },
+        onSuccess: (ctx) => {
+            toast.success(`Welcome to Qurbani Hat, ${data.name}!`);
+            setIsLoading(false);
+            router.push('/');
+            router.refresh();
+        },
+        onError: (ctx) => {
+            toast.error(error);
+            setIsLoading(false);
+        },
+    });
+
+};
