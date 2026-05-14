bc892e6dd190977f0755269330349b35ca6c4a18
Rashedul2001 <hacker360sr@gmail.com>
2026-05-14 18:58:03 +0600

    Minor change to see if vercel redeploy itself or not

--- src/lib/services/SocialSignIn.js
+++ src/lib/services/SocialSignIn.js
@@ -3,8 +3,13 @@ import { authClient } from "../auth-client";
 
 
 export const socialSignIn = async (provider) => {
-    const { data } = await authClient.signIn.social({
+    const { data, error } = await authClient.signIn.social({
         provider: provider,
     });
-
+    if (data) {
+        toast.success('Signed in successfully!');
+    }
+    if (error) {
+        toast.error('Failed to sign in. Please try again.');
+    }
 };
